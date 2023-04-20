9872abe3a043732233b48893b7f5e924ecdcbded
cjones266 <callumjones297@outlook.com>
2023-04-20 20:47:40 +0100

    landing_v1

---
+++ arable_hire/landing.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>ArableHire</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">
+				<img src="logo.png" alt="ArableHire Logo">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Hire</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Advertise</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+    <main class="container">
+    </header>
+    </main>
+<footer>
+  <div class="container">
+    <div class="row">
+        <p>&copy; 2023 ArableHire.</p>
+    </div>
+</div>
+</footer>
+</body>
+</html>

6aa0626ac17a81b8b81b57f95da1b544ceeffe4f
cjones266 <callumjones297@outlook.com>
2023-04-25 19:48:54 +0100

    View form and category view

---
+++ arable_hire/catView.php
@@ -0,0 +1,50 @@
+<!-- This file is used for the categories search -->
+
+<?php
+
+// No logic required as page is visible to all users to browse.
+
+session_start();
+
+include "header.php";
+
+// Include database connection code
+include "config.php";
+
+?>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-3">
+      <?php
+      include "search.php";
+      ?>
+    </div>
+    <div class="col-md-9">
+      <?php
+ 
+      $sql = "SELECT category FROM advertisements";
+
+      $result = mysqli_query($connection, $sql);
+
+      echo '<ul>';
+      while ($row = mysqli_fetch_assoc($result)) {
+          echo '<li><a href="view.php?category=' . urlencode($row['category']) . '">' . $row['category'] . '</a></li>';
+      }
+      echo '</ul>';
+
+      $category = $_GET['category'];
+
+      $query = "SELECT * FROM advertisements WHERE category='$category'";
+
+      $result = mysqli_query($connection, $query);
+
+
+      while ($row = mysqli_fetch_assoc($result)) {
+      }
+
+      ?>
+    </div> <!-- Close the div with class "col-md-9" -->
+  </div> <!-- Close the div with class "row" -->
+</div> <!-- Close the div with class "container-fluid" -->
+
---
+++ arable_hire/viewForm.php
@@ -0,0 +1,114 @@
+<?php
+
+// No logic required as page is visible to all users to browse.
+
+session_start();
+
+include "header.php";
+
+// Include database connection code
+include "config.php";
+
+
+?>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-3">
+        <?php
+        include "search.php";
+        ?>
+    </div>
+    <div class="col-md-9">
+        <?php
+        if (isset($_GET['search'])) {
+            $search = $_GET['search'];
+            $county = $_GET['county'];
+            // Prepare the SQL statement
+            $sql = "SELECT * FROM advertisements WHERE name LIKE '%" . $search . "%'";
+            if (!empty($county)) {
+                $sql .= " AND county = '$county'";
+            }
+            // Execute the query
+            $result = $conn->query($sql);
+            // Check if any results were found
+            if ($result->num_rows > 0) {
+                // Output the table header
+                echo '<table class="table table-striped">';
+                echo '<thead><tr><th>Search Results</th></tr></thead>';
+                // Loop through each result and output it as a table row
+                while ($row = $result->fetch_assoc()) {
+                    echo '<tr>';
+                    echo '<td><a href="viewForm.php?id=' . $row['id'] . '">' . $row['name'] . '</a></td>';
+                    echo '</tr>';
+                }
+                // Output the table footer
+                echo '</table>';
+            } else {
+                echo "No forms found.";
+            }
+        } else {
+
+            $id = $_GET['id'];
+            $forms = "SELECT name, category, description, county, start_date, end_date, cost, email, image FROM advertisements WHERE id = $id";
+            $result = $conn->query($forms);
+            
+            // Display the full form on the PHP page
+            if ($result->num_rows > 0) {
+                // Loop through each result and create a table row for each one
+                while($row = $result->fetch_assoc()) {
+                  // Display the form fields using HTML table rows
+                  echo '<table class="table table-striped">';
+                  echo '<tbody>';
+                  echo '<tr><td><strong>Name:</strong></td><td>' . $row['name'] . '</td></tr>';
+                  echo '<tr><td><strong>Category:</strong></td><td>' . $row['category'] . '</td></tr>';
+                  echo '<tr><td><strong>Description:</strong></td><td>' . $row['description'] . '</td></tr>';
+                  echo '<tr><td><strong>County:</strong></td><td>' . $row['county'] . '</td></tr>';
+                  echo '<tr><td><strong>Available From:</strong></td><td>' . $row['start_date'] . '</td></tr>';
+                  echo '<tr><td><strong>Available Until:</strong></td><td>' . $row['end_date'] . '</td></tr>';
+                  echo '<tr><td><strong>Cost:</strong></td><td>' . $row['cost'] . '</td></tr>';
+                  echo '<tr><td><strong>Email:</strong></td><td>' . $row['email'] . '</td></tr>';
+                  echo '<tr><td><strong>Image:</strong></td><td><img src="data:image/jpeg;base64,' . base64_encode($row['image']) . '" style="width:350px; height:350px;" /></td>';
+                  echo '</tbody>';
+                  echo '</table>';
+                }
+              } else {
+                echo "Form not found.";
+              }
+
+            /* old version
+            // If no new search term is specified, display the full form from the database based on the ID of the form that was clicked
+            $id = $_GET['id'];
+            $forms = "SELECT name, category, description, county, start_date, end_date, cost, email, image FROM advertisements WHERE id = $id";
+            $result = $conn->query($forms);
+            // Display the full form on the PHP page
+            if ($result->num_rows > 0) {
+                // Loop through each result and create a table row for each one
+                while($row = $result->fetch_assoc()) {
+                    // Display the form fields using HTML table rows
+                    echo '<table class="table table-striped">';
+                    echo '<thead><tr><th colspan="2">' . $row['title'] . '</th></tr></thead>';
+                    echo '<tbody>';
+                    echo '<tr><td>Description:</td><td>' . $row['description'] . '</td></tr>';
+                    // Loop through the form fields and add a table row for each one
+                    foreach ($row as $key => $value) {
+                        if ($key != 'id' && $key != 'title' && $key != 'description' && $key != 'image') {
+                            echo '<tr><td>' . $key . ':</td><td>' . $value . '</td></tr>';
+                        }
+                        if ($key == 'image') {
+                            echo '<tr><td>Image:</td><td><img src="data:image/jpeg;base64,'.base64_encode($value).'" /></td></tr>';
+                        }                    
+                    }                    
+                    echo '</tbody>';
+                    echo '</table>';
+                }
+            } else {
+                echo "Form not found.";
+            }
+            */
+
+        }
+        ?>
+    </div>
+  </div>
+</div>
